1076ddede2e59bdcdadc0b33b2e6d38c574bebfe
kdevivanco <kdevivanco@gmail.com>
2023-02-14 18:00:11 -0500

    just images missing

--- automate_translate.py
+++ automate_translate.py
@@ -1,8 +1,40 @@
 import os
 import pdb
 
-js_script = '''</noscript>
-  <script type="text/javascript">
+js_script = '''<script type="text/javascript">
+    const translate = async (text) => {
+      const sourceLang = 'en';
+      const targetLang = 'hi';
+      const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${sourceLang}&tl=${targetLang}&dt=t&q=${encodeURI(text)}`;
+
+      const response = await fetch(url);
+      const data = await response.json();
+
+      if (data[0][0][0]) {
+        return data[0][0][0];
+      } else {
+        return text;
+      }
+    };
+
+
+    let elements = document.querySelectorAll('body strong, h1, a:not(a.hover-no-underline), body span:not(span.icon-medium), body h2, body h3, body button,p, footer a, a.link-gray-underline a.site-header__navigation-action, a.site-header__navigation-submenu-action');
+    for (let element of elements) {
+      if (element.innerText) {
+        const originalText = element.innerText
+        translate(originalText.toString())
+          .then((translatedText) => {
+            element.innerText = translatedText;
+          })
+          .catch((error) => {
+            console.error(error);
+            element.innerText = originalText;
+          });
+      }
+    }
+  </script>'''
+ 
+to_replace='''<script type="text/javascript">
     const translate = async (text) => {
       const sourceLang = 'en';
       const targetLang = 'hi';
@@ -34,8 +66,6 @@ js_script = '''</noscript>
       }
     }
 </script>'''
- 
-to_replace='</noscript>'
 
 def add_to_folder(folder_path):
     for filename in os.listdir(folder_path):
